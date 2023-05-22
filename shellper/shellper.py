406d3a2cbd51daf8a9fdb5c01cfa7fc306fc3159
Miguel FMC <miguelfm95@gmail.com>
2023-05-21 18:33:25 -0700

    Moved prompt generation to separate function and added number option

--- shellper/shellper.py
+++ shellper/shellper.py
@@ -7,20 +7,37 @@ API_KEY = os.getenv("OPENAI_API_KEY")
 openai.api_key = API_KEY
 
 
+def generate_prompt(query, num=None):
+    return f"""
+            List Linux shell commands that would accomplish the task described by the text enclosed by triple backticks:\n"
+            ```{query}```.\n"
+            Output the shell commands in the following format:\n"
+            $ <command 1>\n"
+            $ <command 2>\n"
+            $ ...\n"
+            $ <command n>\n"
+            Limit your output to {str(num)} options or less.\n
+            If the described task does not correspond to any command line action, simply output:\n"
+            'The task your described cannot be accomplished through the command line'\n"
+            Do not add an explanation after generating the commands.
+            """
+
+
 @click.command()
-@click.option("--query", type=str, prompt="What are you trying to do?")
-def search(query):
+@click.argument("query", type=str)
+@click.option("--num", type=int, default=3)
+def search(query, num):
+    prompt = generate_prompt(query, num)
     response = openai.ChatCompletion.create(
-        model="gpt-4",
+        model="gpt-3.5-turbo",
         messages=[
             {
                 "role": "user",
-                "content": f"List Linux shell commands that would accomplish the following: {query}."
-                "Do not add an explanation after generating the commands",
+                "content": prompt,
             }
         ],
-        max_tokens=30,
-        temperature=0.3,
+        max_tokens=64,
+        temperature=0.0,
     )
     commands = response.choices[0].message.content
     click.echo(f"\nHere are some Linux shell commands that might help:\n\n{commands}\n")
